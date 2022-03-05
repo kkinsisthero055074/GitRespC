a0bd83d78148f42c0111dc17a347ec1f06d6a241
kkinsisthero055074 <1229373795@qq.com>
2022-03-05 11:23:13 +0100

    size of data

---
+++ 20220305/20220305/typeOfData.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("char: ");
+	printf("%d\n", sizeof(char));
+
+	printf("short: ");
+	printf("%d\n", sizeof(short));
+
+	printf("int: ");
+	printf("%d\n", sizeof(int));
+
+	printf("long: ");
+	printf("%d\n", sizeof(long));
+
+	printf("long long: ");
+	printf("%d\n", sizeof(long long));
+
+	printf("float: ");
+	printf("%d\n", sizeof(float));
+
+	printf("double: ");
+	printf("%d\n", sizeof(double));
+}
