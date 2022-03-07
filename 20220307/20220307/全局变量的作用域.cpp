20e0506d217a6fc6c18d4a0c277f2a433a2b41ff
kkinsisthero055074 <1229373795@qq.com>
2022-03-07 10:23:19 +0100

    全局变量和局部变量的作用域

--- 20220307/20220307/全局变量的作用域.cpp
+++ 20220307/20220307/全局变量的作用域.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 //当局部变量和全局变量重名时，优先使用局部变量
+//局部变量的作用域就是局部变量所在的范围({}为范围标识),除了范围就不是识别
 int global = 100;
 int main()
 {
@@ -9,5 +10,14 @@ int main()
 	scanf_s("%d", &global);
 
 	printf("the value of global is %d\n", global);
+	return 0;
+
+	{
+		int a = 9;
+		printf("a=%d", a);
+		//在{}范围外就不识别变量a
+	}
+	
+
 	return 0;
 }
