0eeeb38b20bbacbde70d266440de795296aa0d48
kkinsisthero055074 <1229373795@qq.com>
2022-03-14 10:32:48 +0100

    renew

--- 20220307/20220307/tutorial.cpp
+++ 20220307/20220307/tutorial.cpp
@@ -71,7 +71,6 @@ void constant()
 	//3枚举常量，可以在{}赋初值，如果不赋初值，会默认从0开始的常量
 
 
-
 }
 
 
@@ -113,10 +112,136 @@ void String()
 
 }
 
+
+//操作符
+void Operator()
+{
+	//~按位取反
+	int a = 0;
+	printf("%d\n",~a);
+
+	//返回的值是-1
+	//正整数的原码、反码、补码是一致的
+	//反码=原码符号位不变，其他位取反
+	//补码=反码+1
+	//整数在内存中存的是反码
+
+	int b = 1;
+	int c;
+	c = (++b) + (++b) + (++b);
+	printf("c=%d\n", c);
+	//不建议这样使用，不同的编译器会产生不同的结果
+
+
+	//强制类型转换
+	int d = (int)3.141592658;
+	char c3 = 121;
+	printf("d=%d\n", d);
+	printf("c=%c\n", c3);
+
+	//逗号表达式
+	int da = 0;
+	int db = 3;
+	int dc = 5;
+	int dd = (da = db + 2, dc = da - 4, db = dc + 2);
+	printf("dd=%d\n", db);
+	//结果为3
+	//逗号表达式即是把括号中所有的表达式都执行完，取最后一个表达式的值
+
+
+
+
+
+}
+
+
+//重新定义关键字
+//关键字
+void Key()
+{
+	for (int i = 1; i < 5; i++)
+	{
+		for (int j = 1; j < 5; j++)
+		{
+			int a = 10;
+			a = a + i * 10 + j;
+			printf("a=%d ", a);
+		}
+	}
+	printf("\n");
+
+	for (int i = 1; i < 5; i++)
+	{
+		for (int j = 1; j < 5; j++)
+		{
+			static int a = 10;			
+			a = a + i * 10 + j;
+			printf("a=%d ", a);
+		}
+	}
+	printf("\n");
+	//静态局部变量一直存在，回去后跳过定义
+	//超过其作用域还是不能用
+
+	
+}
+
+//define是一个预处理指令
+//1 define定义符号
+#define MIN 100
+//2. define定义的宏
+#define ADD(X,Y) X+Y
+#define ADD2(X,Y) (X+Y)
+void Define()
+{
+	printf("MIN=%d\n", MIN);
+	printf("%d\n", 4*ADD(2,3));
+	printf("%d\n", 4 * ADD2(2, 3));
+	//上面案例说明define定义宏是直接替换的
+}
+
+//指针
+void Pointer()
+{
+	int num = 20;
+	int *p = &num;
+	*p = 30;
+	printf("%d\n", num);
+	printf("%d\n", sizeof(p));
+	//说明：所有类型占内存的空间都是四个字节
+}
+
+//结构体
+struct Student
+{
+	char name[20];//C语言中没有字符串类型
+	int age;
+	double score;
+
+};
+void Struct()
+{
+	struct Student s1 = { "kangkang",32,82.36 };
+	printf("%s %d %f\n", s1.name, s1.age, s1.score);
+	
+	struct Student* pa = &s1;
+	printf("%s %d %f\n", pa->name, pa->age,pa->score);
+
+
+
+}
+
 int main()
 {
 	//variable();
 	//constant();
-	String();
+	//String();
+	//Operator();
+	//Key();
+	//Define();
+	//Pointer();
+	Struct();
+
+
 	return 0;
 }
